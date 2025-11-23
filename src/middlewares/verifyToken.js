ac9fd4b99201a6a01d4050080fb6aa4a58a9f1df
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2025-11-23 20:57:01 +0600

    Create Verify Token function

---
+++ src/middlewares/verifyToken.js
@@ -0,0 +1,29 @@
+import { User } from "../models/user.model";
+import apiErrors from "../utils/apiErrors";
+import jwt from "jsonwebtoken";
+
+const verifyToken = async (req, res, next) => {
+  try {
+    const token =
+      req.cookies.accessToken || req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      throw new apiErrors(401, "Unauthorized: No token provided in cookie");
+    }
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decoded._id).select("_id email name role");
+    if (!user) {
+      throw new apiErrors(401, "Unauthorized: User Not found");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    return next(
+      new apiErrors(
+        error.status || 500,
+        error.message || "Internal Server Error"
+      )
+    );
+  }
+};
+
+export default verifyToken;
