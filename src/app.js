dd52e13edb7ee498e8aec5fcbc5d58e71a9d90b8
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2025-11-26 14:48:58 +0600

    add base url for payment route

--- src/app.js
+++ src/app.js
@@ -30,11 +30,13 @@ import userRoute from "./routes/user.route.js";
 import templateRoute from "./routes/template.route.js";
 import generationHistoryRoute from "./routes/generationHistory.route.js";
 import subscriptionPlanRoute from "./routes/subscriptionPlan.route.js";
+import paymentRoute from "./routes/payment.route.js";
 
 app.use("/api/v1/auth", userRoute);
 app.use("/api/v1/template", templateRoute);
 app.use("/api/v1/history", generationHistoryRoute);
 app.use("/api/v1/subscription", subscriptionPlanRoute);
+app.use("/api/v1/payment", paymentRoute);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "API is running" });
