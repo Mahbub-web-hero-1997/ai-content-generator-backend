ab06b627bce02eee0d6a9b8c5761922a34b45344
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2025-11-27 18:51:13 +0600

    update route

--- src/routes/subscriptionPlan.route.js
+++ src/routes/subscriptionPlan.route.js
@@ -3,7 +3,6 @@ import {
   createSubscriptionPlan,
   deleteSubscriptionPlan,
   getAllSubscriptionPlans,
-  getOwnSubscriptionPlan,
   getSingleSubscriptionPlan,
   updateSubscriptionPlan,
 } from "../controllers/subscriptionPlan.controller.js";
@@ -12,7 +11,6 @@ import verifyToken from "../middlewares/verifyToken.js";
 const router = new Router();
 router.route("/create").post(createSubscriptionPlan);
 router.route("/all").get(verifyToken, getAllSubscriptionPlans);
-router.route("/all/:id").get(verifyToken, getOwnSubscriptionPlan);
 router.route("/single/:id").get(verifyToken, getSingleSubscriptionPlan);
 router.route("/update/:id").put(verifyToken, updateSubscriptionPlan);
 router.route("/delete/:id").delete(verifyToken, deleteSubscriptionPlan);
