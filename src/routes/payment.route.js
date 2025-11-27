5d65a62ac4a24d500e3020b99391f7bed35575af
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2025-11-27 12:24:25 +0600

    add wonPayments route

--- src/routes/payment.route.js
+++ src/routes/payment.route.js
@@ -3,6 +3,7 @@ import {
   createPayment,
   deletePayment,
   getAllPayments,
+  getOwnPayments,
   getPaymentById,
   verifyPayment,
 } from "../controllers/payment.controller.js";
@@ -11,9 +12,10 @@ import verifyToken from "../middlewares/verifyToken.js";
 const router = new Router();
 
 router.route("/create-checkout-session").post(verifyToken, createPayment);
-router.route("verify").get(verifyToken, verifyPayment);
+router.route("/verify").get(verifyToken, verifyPayment);
 router.route("/all").get(verifyToken, getAllPayments);
+router.route("/own-payments").get(verifyToken, getOwnPayments);
 router.route("/single/:id").get(verifyToken, getPaymentById);
-router.route("/delete").delete(verifyToken, deletePayment);
+router.route("/delete/:id").delete(verifyToken, deletePayment);
 
 export default router;
